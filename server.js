524cb88d9663f5b24dabcc6a3749958cf59b7243
Pervejhasan <mdpervej12441@gmail.com>
2023-08-18 15:19:53 +0600

    application connect with database

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express=require('express')
-
+const mongoose=require('mongoose')
 const app=express();
 const port=process.env.PORT ||3000;
 
@@ -7,7 +7,21 @@ const port=process.env.PORT ||3000;
 app.get('/',(req,res)=>{
     res.send("Hello Node API");
 })
+app.get('/blog',(req,res)=>{
+    res.send("Hello Blog ");
+})
+
+mongoose.
+connect(`mongodb+srv://admin:1911512441@nodeapi.69ubeik.mongodb.net/Node-API?retryWrites=true&w=majority`)
+.then(()=>{
+    console.log("connected to mongodb")
+})
+.catch((err)=>{
+console.log(err);
+})
+
 
 app.listen(port,()=>{
     console.log(`application listening port ${port} and running http://localhost:${port}`)
 })
+
