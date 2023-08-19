4088011a1e34ca8b1697351d89945429bd221070
Pervejhasan <mdpervej12441@gmail.com>
2023-08-19 10:34:10 +0600

    edit data

--- server.js
+++ server.js
@@ -5,18 +5,19 @@ const port=process.env.PORT ||3000;
 const Product = require('./model/productModels');
 // json middleware
 app.use(express.json())
-
+app.use(express.urlencoded({extended:false}))
+// add data 
 app.post ('/products', async(req,res)=>{
  try {
     const product=await Product.create(req.body)
     res.status(200).json(product)
-    
  } catch (error) {
     console.log(error.message);
     res.status(500).json({message:error.message})
  }
     })
 
+//read data
 app.get('/products',async(req,res)=>{
 try {
   const products=await Product.find({});
@@ -26,19 +27,33 @@ try {
 }
 })
 
+//read single data by id
 app.get('/products/:id',async (req,res)=>{
 try {
     // console.log(typeof req.params)
     const{id}=req.params;
     const product=await Product.findById(id)
     res.status(200).json(product)
-  
 } catch (err) {
     res.status(500).json({message:err.message})
 }
-
 })
 
+//update or edit data
+
+app.put('/products/:id', async (req,res)=>{
+try {
+const {id}=req.params;
+const product= await Product.findByIdAndUpdate(id, req.body)
+    if(!product){
+        return res.status(404).json({message:`cannot find any product with id ${id} `})
+    }
+    const updatedProduct=await Product.findById(id)
+ res.status(200).json(updatedProduct)
+} catch (err) {
+    res.status(500).json({message:err.message})
+}
+})
 
 
 /* =========> first we connect the application with database,then we run the application <============*/
